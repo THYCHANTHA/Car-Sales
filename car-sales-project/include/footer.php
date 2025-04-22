b41a7676157079863bc0ae1befacb603a3525547
Thy Chantha <157097678+THYCHANTHA@users.noreply.github.com>
2025-04-23 01:20:40 +0700

    Add car-sales-project/include/footer.php 1745346025677

---
+++ car-sales-project/include/footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer class="bg-white shadow p-4 mt-8 text-center text-gray-600">
+        &copy; <?php echo date('Y'); ?> Car Sales. All rights reserved.
+    </footer>
+</body>
+</html>
